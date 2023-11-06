6e804cd2a8638ecd9bb5f0cc7114acc4ad845654
cloudsonika <113979766+cloudsonika@users.noreply.github.com>
2023-11-06 12:23:35 +0530

    Update configure-newrelic.sh

--- scripts/configure-newrelic.sh
+++ scripts/configure-newrelic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set your New Relic license key
-sudo nrsysmond-config --set license_key=YOUR_LICENSE_KEY
+sudo nrsysmond-config --set license_key=ae4f67198be65ab783366eb38e896e27FFFFNRAL
 
 # Start the New Relic agent
 sudo systemctl start newrelic-sysmond
