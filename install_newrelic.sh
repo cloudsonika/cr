6b927656224c2b60b38987130d406e66c03cf0e3
cloudsonika <113979766+cloudsonika@users.noreply.github.com>
2023-11-07 10:25:44 +0530

    Create install_newrelic.sh

---
+++ install_newrelic.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Add New Relic GPG key
+curl -s https://download.newrelic.com/548C16BF.gpg | sudo apt-key add -
+
+# Add New Relic APT repository
+sudo sh -c 'echo "deb http://apt.newrelic.com/debian/ newrelic non-free" > /etc/apt/sources.list.d/newrelic.list'
+
+# Update package list
+sudo apt-get update
+
+# Install New Relic Infrastructure Agent
+sudo apt-get install newrelic-infra -y
+
+# Set your New Relic license key
+sudo newrelic-infra-config set license_key=$1
+
+# Start the New Relic Infrastructure Agent
+sudo systemctl start newrelic-infra
